f41be84b9dc221705599e65bfa259d65d23c87d1
Richard Zuikov <astroricards@gmail.com>
2025-12-20 21:47:22 +0100

    TRYING TO FIX THIS BACKEND

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,19 +1,9 @@
 from flask import Flask
-from flask_login import LoginManager
 from .models import db
-from .routes import main
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "dev"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite3"
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@db:5432/deineDB'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-
-    login_manager = LoginManager()
-    login_manager.login_view = "main.login"
-    login_manager.init_app(app)
-
-    app.register_blueprint(main)
-
     return app
--- backend/src/models.py
+++ backend/src/models.py
@@ -3,13 +3,14 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class User(db.Model, UserMixin):
-    __tablename__ = "users"
-
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    role = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    role = db.Column(db.String(50))
+
+
 
 class Ticket(db.Model):
     __tablename__ = "tickets"
